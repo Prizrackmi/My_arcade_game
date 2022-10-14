07291bc34370ac60f9a350c0e2753b9471074cf3
Алексей Савко <114352731+Prizrackmi@users.noreply.github.com>
2022-10-14 23:07:15 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        # display param
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 32, 192)
+        self.ship_speed = 1.5
